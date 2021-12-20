b2c4207a5d50261b71108876a88c27366ffd49a5
s4-hub <syafii.newbie@gmail.com>
2021-12-20 08:48:01 +0700

    Fixing api

--- static/kepesertaan/js/bidang.js
+++ static/kepesertaan/js/bidang.js
@@ -1,20 +1,9 @@
-
-function chainSelect(current, target){
-    var value1 = $(current).on('change', function(){
-      if($(this).find(':selected').val() != ''){
-        $(target).removeAttr('disabled');
-        var value = $(this).find(':selected').text();
-      }else{
-        $(target).prop('disabled', 'disabled').val(null);
-      }
-    return value;
-    });
-    return value1;
+var pembinaQuery = `
+query{
+  allProfile{
+    username{
+      username
+    }
   }
-  bidang = chainSelect('select#bidang', '#makeselect');
-  //color = chainSelect('select#color', '#qty');
-  jabatan = chainSelect('select#makeselect', '#daftar_pembina_btn');
-  
- console.log(bidang)
- console.log(jabatan)  
-  
+}
+`
