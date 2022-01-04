92527180d762c26510323bbaae72b122bc6e936d
liveshare-hub <syafii.newbie2@gmail.com>
2022-01-04 17:11:57 +0700

    fixing apps

--- kepesertaan/context_processors.py
+++ kepesertaan/context_processors.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 
 def info_context(request):
     try:
-        kepala = Profile.objects.select_related('username','jabatan').filter(Q(jabatan__pk=1) | Q(jabatan__pk=2),username__username=request.user)
+        kepala = Profile.objects.select_related('username','jabatan').filter(Q(jabatan__pk=1) | Q(jabatan__pk=2) | Q(jabatan__pk=3),username__username=request.user)
         if kepala.exists:
             datas = Informasi.objects.all().order_by('-created')[:5]
             return {'info':datas, 'kepala':kepala}
