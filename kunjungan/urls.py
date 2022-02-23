9c611025c46a7987c5f488a27c44243082ded2b2
s4-hub <syafii.newbie@gmail.com>
2022-02-23 21:17:50 +0700

    fixing

--- kunjungan/urls.py
+++ kunjungan/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.kunjungan, name='kunjungan-list'),
     path('buat/', views.simpan_kunjungan, name='buat-kunjungan'),
-    path('detil/<int:pk>/', views.detail_kunjungan, name='detil-kunjungan'),
+    # path('detil/<int:pk>/', views.detail_kunjungan, name='detil-kunjungan'),
+    path('detil/<int:pk>', views.docPDF, name='detil-kunjungan'),
     path('approval/list/', views.daftar_approval_kunjungan, name='list-approval'),
     path('approval/<int:pk>/done/', views.approval_kunjungan, name='approval-kunjungan'),
     path('pdf/<int:pk>/', views.GeneratePDF.as_view(), name='generate-pdf'),
