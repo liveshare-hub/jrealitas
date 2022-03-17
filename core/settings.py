4b987af8da7560432c86e8fa44cf443eb6c8fc1b
liveshare-hub <syafii.newbie2@gmail.com>
2022-03-17 13:47:50 +0700

    fixig

--- core/settings.py
+++ core/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 import os
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -102,17 +103,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':config('DBNAME'),
-        'USER':config('DBUSER'),
-        'PASSWORD':config('DBPASS'),
-        'HOST':config('DBHOST'),
-        'PORT':'',
-        'OPTIONS':{
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE':'django.db.backends.mysql',
+        # 'NAME':config('DBNAME'),
+        # 'USER':config('DBUSER'),
+        # 'PASSWORD':config('DBPASS'),
+        # 'HOST':config('DBHOST'),
+        # 'PORT':'',
+        # 'OPTIONS':{
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        # }
        
     }
 }
@@ -234,3 +235,7 @@ EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+
+SESSION_EXPIRE_SECONDS = 3600  # 5 min
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = '/accounts/login/'
